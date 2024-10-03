6e13b002172924452ba8efb0a046b9ad10a59719
boadubakim <bakim.boadu@georgebrown.ca>
2024-10-03 12:56:24 -0400

    ,gitignore added

---
+++ build/precache-manifest.d7eeae9da4e2901ebe6c0619bb73031d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "03cf9aae8963a29e455390dac1b754a0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fbd96bb439fb004adc21",
+    "url": "/static/css/main.30d17bd7.chunk.css"
+  },
+  {
+    "revision": "68f45c95e57e64b216d9",
+    "url": "/static/js/2.6fd8b6a4.chunk.js"
+  },
+  {
+    "revision": "60f6bf9e100e456690e9ab6c9a37bfc2",
+    "url": "/static/js/2.6fd8b6a4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4b79128c0b7f63005ad3",
+    "url": "/static/js/3.34f6ea70.chunk.js"
+  },
+  {
+    "revision": "fbd96bb439fb004adc21",
+    "url": "/static/js/main.4ff140af.chunk.js"
+  },
+  {
+    "revision": "f840c2b9540ef480668e",
+    "url": "/static/js/runtime-main.a3c98b42.js"
+  }
+]);
