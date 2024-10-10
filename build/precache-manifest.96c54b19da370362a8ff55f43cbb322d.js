2f4f143b5ec950c9560ed89020963cbb27687d11
boadubakim <bakim.boadu@georgebrown.ca>
2024-10-10 10:39:03 -0400

    added title

---
+++ build/precache-manifest.96c54b19da370362a8ff55f43cbb322d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "36bde24b85c97ee48f6ca199fdbed092",
+    "url": "/Comp3104/index.html"
+  },
+  {
+    "revision": "fbd96bb439fb004adc21",
+    "url": "/Comp3104/static/css/main.30d17bd7.chunk.css"
+  },
+  {
+    "revision": "68f45c95e57e64b216d9",
+    "url": "/Comp3104/static/js/2.6fd8b6a4.chunk.js"
+  },
+  {
+    "revision": "60f6bf9e100e456690e9ab6c9a37bfc2",
+    "url": "/Comp3104/static/js/2.6fd8b6a4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4b79128c0b7f63005ad3",
+    "url": "/Comp3104/static/js/3.34f6ea70.chunk.js"
+  },
+  {
+    "revision": "fbd96bb439fb004adc21",
+    "url": "/Comp3104/static/js/main.4ff140af.chunk.js"
+  },
+  {
+    "revision": "27b9921acbc2d43535f9",
+    "url": "/Comp3104/static/js/runtime-main.506a14b0.js"
+  }
+]);
